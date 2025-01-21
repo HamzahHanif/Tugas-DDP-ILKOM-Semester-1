66fee4ed58998a9377cc17bb2d1f89df8c167d0a
Hamzah Hanif <92318246+HamzahHanif@users.noreply.github.com>
2025-01-21 23:49:18 +0700

    Update 2117051032_HamzahHanif_Pretest1.cpp

--- 2117051032_HamzahHanif_Pretest1.cpp
+++ 2117051032_HamzahHanif_Pretest1.cpp
@@ -9,7 +9,7 @@ int main (){
 	getline(cin,npm);
 	getline(cin,alamat);
 	
-	cout<<"Nama saya \""<<nama<<"\" NPM "<<npm<<" dan beralamat di "<<alamat;
+	cout<<"Nama saya \""<<nama<<"\" NPM "<<npm<<" dan beralamat di   "<<alamat;
 	return 0;
 
 	
